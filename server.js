c7ddee6ef98cc131a37cdfa282e9b6d81d484d4d
swayambita <swayambita@ccs.neu.edu>
2017-07-12 14:17:34 -0400

    basic structure

---
+++ server.js
@@ -0,0 +1,13 @@
+var app = require('./express');
+var express = app.express;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+//require("./test/app");
+
+app.listen(3000);
